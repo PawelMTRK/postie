c618fc13dcfa411de50b78a67bf03b412692ff11
PawelMTRK <146580769+PawelMTRK@users.noreply.github.com>
2025-09-26 21:44:00 +0200

    tons of setup
    - add sqlite connection
    - add tailwind bundle
    - add login/register page (bless symfony cli)
    - create template for main feed

---
+++ src/Controller/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\Type\UserType;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $plainPass = $user->getPassword();
+            $hashedPass = $passwordHasher->hashPassword(
+                $user,
+                $plainPass
+            );
+            $user->setPassword($hashedPass);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirect('/');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
