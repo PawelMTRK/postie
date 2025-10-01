58b14d0aadc16a647c89c17674d43a056b1229a4
PawelMTRK <146580769+PawelMTRK@users.noreply.github.com>
2025-10-01 16:46:19 +0200

    feat: change post form type and post rendering newlines

--- src/Form/Type/PostType.php
+++ src/Form/Type/PostType.php
@@ -5,7 +5,7 @@ namespace  App\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use App\Entity\Topic;
 
@@ -14,11 +14,11 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextType::class)
+            ->add('content', TextareaType::class)
             ->add('topic', EnumType::class, [
                 'class' => Topic::class,
             ])
-            ->add('save', SubmitType::class)
+            ->add('post', SubmitType::class)
             ;
     }
 }
